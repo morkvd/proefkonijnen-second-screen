16360353d693d5b48e2613d3e16aba7d8fae2b13
morkvd <markvdijken@gmail.com>
2016-10-25 17:04:40 +0200

    add navigation

--- js/main.js
+++ js/main.js
@@ -1,37 +1,47 @@
+const emitNavigationEvent = function (location) {
+  this.$emit('navigate', location);
+};
+
+
 const VM = new Vue({
   el: '#proefkonijnen',
   data: {
-    currentComponent: null,
-    componentsArray: ['live', 'diy', 'question']
-  },
-
-  components: {
-
-    live : {
-      data: function() {
-        return { title: 'speel live mee' };
-      },
-      template: '<h1>{{ title }}</h1>',
-    },
-
-    diy: {
-      data: function() {
-        return { title: 'doe het lekker zelf' };
-      },
-      template: '<h1>{{ title }}</h1>',
-    },
-
-    question: {
-      data: function() {
-        return { title: 'bezopen vragen' };
-      },
-      template: '<h1>{{ title }}</h1>',
-    },
+    currentView: 'home',
   },
 
   methods: {
-    swapComponent: function(component) {
-      this.currentComponent = component;
+    changeView: function (newView) {
+      this.currentView = newView;
+    }
+  },
+
+  components: {
+    home: {
+      methods: {
+        navigate: emitNavigationEvent,
+      },
+      template: '#main_menu_template'
+    },
+
+    live: {
+      methods: {
+        navigate: emitNavigationEvent,
+      },
+      template: '#live_template'
+    },
+
+    diy: {
+      methods: {
+        navigate: emitNavigationEvent,
+      },
+      template: '#diy_template'
+    },
+
+    questions: {
+      methods: {
+        navigate: emitNavigationEvent,
+      },
+      template: '#questions_template'
     }
   }
 });
